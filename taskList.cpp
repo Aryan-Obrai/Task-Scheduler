bd7bcb963032f172654895ce593f62ae98eb42fe
Rancho-Relaxo <ians.lambs@gmail.com>
2022-05-15 21:56:10 -0700

    Fixed setClass() in taskList header and added base of taskList.cpp

---
+++ taskList.cpp
@@ -0,0 +1,54 @@
+#include "task.h"
+#include "work.h"
+#include "study.h"
+#include "personal.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+TaskList::TaskList() {
+    taskName = "Untitled List";
+    description = "N/A";
+    priority = 0;
+    classification = "N/A";
+    dueTime = "01-01-2000, 12:00 AM"
+    complete = false;
+
+    allComplete = false;
+}
+
+TaskList::TaskList(string name, string desc, int prio, string dueDate, string dueClock, string className, string workLink) {
+    taskName = name + " List";
+    description = desc;
+    priority = prio;
+    classification = "List";
+    dueTime = dueDate + ", " + dueClock;
+    complete = false;
+
+    allComplete = false;
+}
+
+void TaskList::printTask() {
+    cout << "Name: " << getName() << endl
+         << "Description: " << getDesc() << endl
+         << "Priority: " << getPriority() << endl
+         << "Due on: " << getDue() << endl
+         << "List Complete: " << (allComplete ? "True" : "False") << endl;
+}
+
+void TaskList::printSubTasks() {
+
+}
+
+void TaskList::setClass(Task* oldTask, char class) {
+    
+}
+
+void TaskList::addSubTask(Task* _Task) {
+    SubTasks.push_back(_Task);
+}
+
+void TaskList::deleteSubTask(Task* _Task) {
+    
+}
