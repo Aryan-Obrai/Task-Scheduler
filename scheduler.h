6ac68fa37821e469e73547cbfce93298b86f2590
Ares278 <hobra001@ucr.edu>
2022-05-15 22:09:56 -0700

    Changed hpp to .h for scheduler header

---
+++ scheduler.h
@@ -0,0 +1,27 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Scheduler: public Task {
+  private:
+    vector<Task> fullList;
+  public:
+    void display();
+    void addTask(Task*);
+    void removeTask(Task*);
+    void sortTasks(char);
+    void editDesc(Task*, string);
+    void editDue(Task*, string, string);
+    void editPrio(Task*, int);
+    void markComplete(Task*);
+    void markIncomplete(Task*);
+    void sort(char, char);
+    void undo();
+};
+
+#endif
+
