b1081bb5286a25fd94ba01f974bec3571c45439e
Rancho-Relaxo <ians.lambs@gmail.com>
2022-05-16 18:25:58 -0700

    Updating scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -1,27 +1,32 @@
-#ifndef SCHEDULER_H
-#define SCHEDULER_H
+#ifndef __SCHEDULER_H__
+#define __SCHEDULER_H__
 
+#include "task.h"
+#include "work.h"
+#include "study.h"
+#include "personal.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Scheduler: public Task {
   private:
+    char sortType;
     vector<Task> fullList;
   public:
     void display();
-    void addTask(Task*);
-    void removeTask(Task*);
-    void sortTasks(char);
-    void editDesc(Task*, string);
-    void editDue(Task*, string, string);
-    void editPrio(Task*, int);
-    void markComplete(Task*);
-    void markIncomplete(Task*);
-    void sort(char, char);
+    void addTask(Task* );
+    void removeTask(Task* );
+    void sortTasks(char );
+    void editDesc(Task* , string );
+    void editDue(Task* , string , string );
+    void editPrio(Task* , int );
+    void markComplete(Task* );
+    void markIncomplete(Task* );
+    void sort();
     void undo();
 };
 
 #endif
-
