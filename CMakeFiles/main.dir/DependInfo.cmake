115f6bb70ca7fb42b32f5ef1808913e7de39ff30
Rancho-Relaxo <ians.lambs@gmail.com>
2022-05-28 18:32:43 -0700

    Created CMakeLists.txt and makefiles

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/main.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/main.dir/main.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/personal.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/main.dir/src/personal.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/scheduler.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/main.dir/src/scheduler.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/study.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/main.dir/src/study.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/task.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/main.dir/src/task.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/taskList.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/main.dir/src/taskList.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/work.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/main.dir/src/work.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/personal.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/test.dir/src/personal.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/scheduler.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/test.dir/src/scheduler.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/study.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/test.dir/src/study.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/task.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/test.dir/src/task.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/taskList.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/test.dir/src/taskList.cpp.o"
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/src/work.cpp" "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/CMakeFiles/test.dir/src/work.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest/googletest/include"
+  "googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/csmajs/ilamb004/final-project-andres-aryan-harjyot-ian/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
