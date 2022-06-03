c2ac9cda42ec1e419069fa13fc14b818588714f2
Rancho-Relaxo <ians.lambs@gmail.com>
2022-06-02 19:19:24 -0700

    Fixes to main

--- src/study.cpp
+++ src/study.cpp
@@ -1,7 +1,8 @@
 #include "../header/study.h"
 
 using namespace std;
-Study::Study(){
+
+Study::Study() {
     taskName = "Untitled";
     description = "N/A";
     priority = 0;
@@ -9,28 +10,34 @@ Study::Study(){
     dueTime = "01-01-2000, 12:00 AM";
     complete = false;
 }
+
 Study::Study(string name, string desc, int prio, string dueDate, string dueClock): Task(name, desc, prio, dueDate, dueClock){};
+
 void Study::printTask(){
-cout << "Task Name: " << getName() << endl;
-cout << "Description: " << getDesc() << endl;
-cout << "Priority: " << getPriority() << endl;
-cout << "Classification: " << getClass() << endl;
-cout << "Due on: " << getDue() << endl;
-cout << "Complete: " << (complete ? "True" : "False") << endl;
+    cout << "Task Name: " << getName() << endl;
+    cout << "Description: " << getDesc() << endl;
+    cout << "Priority: " << getPriority() << endl;
+    cout << "Classification: " << getClass() << endl;
+    cout << "Due on: " << getDue() << endl;
+    cout << "Complete: " << (complete ? "True" : "False") << endl;
 }
-void Study::addResources(string res ){
-resources.push_back(res);
+
+void Study::addResources(string res ) {
+    resources.push_back(res);
 }
-void Study::addNotes(string note ){
-notes.push_back(note);
+
+void Study::addNotes(string note ) {
+    notes.push_back(note);
 }
-void Study::printResources(){
+
+void Study::printResources() {
    cout << "Resources: " << endl;
    for(int i = 0; i < resources.size(); i++){
       cout << i+1 << ". " << resources.at(i) << endl;
    }  	
 }
-void Study::printNotes(){
+
+void Study::printNotes() {
    cout << "Notes: " << endl;
    for(int i = 0; i < notes.size(); i++){
       cout << i+1 << ". " << notes.at(i) << endl;
